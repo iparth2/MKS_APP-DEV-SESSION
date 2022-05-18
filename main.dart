b7867b8b3439b02529e8e01c607f97e1815bd56c
akshat <parthgarg7300gmail.com>
2022-05-18 12:28:59 +0530

    Assignment Completed

---
+++ main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('JU Makerspace APP DEV SESSION '),
+        ),
+        body: Center(
+          child: Text('Parth Garg'),
+        ),
+      ),
+    ),
+  );
+}
